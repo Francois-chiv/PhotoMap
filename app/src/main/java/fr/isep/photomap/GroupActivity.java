e152b5c66729b7a5230a13db7d417af9babde49e
davidlvn <lamydavid38@gmail.com>
2021-12-15 08:51:02 +0100

    Added group page

--- app/src/main/java/fr/isep/photomap/GroupActivity.java
+++ app/src/main/java/fr/isep/photomap/GroupActivity.java
@@ -2,14 +2,30 @@ package fr.isep.photomap;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.Source;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class GroupActivity extends AppCompatActivity {
+
+    private List<Map<String, Object>> groups = new ArrayList<Map<String, Object>>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +38,7 @@ public class GroupActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Intent intent = new Intent();
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.marker:
                         intent = new Intent(bottomNavigationView.getContext(), MarkerActivity.class);
                         startActivity(intent);
@@ -39,5 +55,32 @@ public class GroupActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("group")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                //Log.d("Data read", document.getId() + " => " + document.getData());
+                                groups.add(document.getData());
+                            }
+                            RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+                            // set a LinearLayoutManager with default vertical orientation
+                            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+                            recyclerView.setLayoutManager(linearLayoutManager);
+                            //  call the constructor of CustomAdapter to send the reference and data to Adapter
+                            GroupAdapter groupAdapter = new GroupAdapter(groups);
+                            recyclerView.setAdapter(groupAdapter); // set the Adapter to RecyclerView
+                        } else {
+                            Log.w("Data read error", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+
     }
+
 }
